ace3c6cd4279a31fecf4c7ce78c3bffbf2ac218a
John Britos <johnbritosash@gmail.com>
2025-01-19 17:27:35 +0800

    used id

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach((product) => {
           </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart"
-          data-product-name="${product.name}">
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`
@@ -59,11 +59,11 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click', () => {
-            const productName = button.dataset.productName;
+            const productId = button.dataset.productId;
             let similarItem;
 
             cart.forEach((item) => {
-                if(productName === item.productName){
+                if(productId === item.productId){
                     similarItem = item;
                 }
             });
@@ -72,7 +72,7 @@ document.querySelectorAll('.js-add-to-cart')
                 similarItem.quantity += 1;
             }else{
                 cart.push({
-                    productName: productName,
+                    productId: productId,
                     quantity: 1
                 });
             }
